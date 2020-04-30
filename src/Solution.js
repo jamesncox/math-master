8bd735f5863db739c9bb918e97a622b978498e2a
jamesncox <jamesncox@yahoo.com>
2020-04-30 15:55:08 -0500

    Add Solution.js

---
+++ src/Solution.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { INCREASE_SCORE, REDUCE_SCORE, CLEAR_PROBLEM } from './actionTypes'
+
+class Solution extends Component {
+
+    revealSolution = () => {
+        if (this.props.userAnswer !== "") {
+            if (this.props.answer === parseInt(this.props.userAnswer)) {
+                this.props.increaseScore()
+                this.props.clearProblem()
+                return <h4>{this.props.userAnswer} is correct! </h4>
+            } else {
+                this.props.reduceScore()
+                this.props.clearProblem()
+                return <h4 style={{ backgroundColor: "red" }}>Sorry, the answer is {this.props.answer} </h4>
+            }
+        }
+    }
+
+    render() {
+        return (
+            <>
+                {this.revealSolution()}
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    operator: state.math.operator,
+    answer: state.math.answer,
+    userAnswer: state.math.userAnswer
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    increaseScore: () => dispatch({ type: INCREASE_SCORE }),
+    reduceScore: () => dispatch({ type: REDUCE_SCORE }),
+    clearProblem: () => dispatch({ type: CLEAR_PROBLEM })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Solution)
