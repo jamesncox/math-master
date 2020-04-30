a52b8e536a29cef217586007f357f3ca0dcdf489
jamesncox <jamesncox@yahoo.com>
2020-04-30 15:44:26 -0500

    Update App.js/Index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MathContainer from './MathContainer'
+import "./stylesheets/styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Math Master</h1>
+      <MathContainer />
+    </div>
+  );
+}
